8a3c705e27f0230bc2c9fb4bd8d96527a5781b74
lucasolmeta <lolmetab@gmail.com>
2024-10-01 21:28:08 -0500

    improved user experience

--- script.js
+++ script.js
@@ -26,25 +26,26 @@ function displayData(data){
   document.getElementById("outputArea").style.display = 'block';
 
   if(data.length==undefined){
-    document.getElementById("outputArea").innerHTML = "No Poems Found" + "<br>" + "<br>"; 
+    document.getElementById("outputArea").innerHTML = "<b>No Poems Found</b>"; 
+    return;
   } else if(data.length==1){
-    document.getElementById("outputArea").innerHTML = "<b>1 Poem Found</b>" + "<br>" + "<br>"; 
+    document.getElementById("outputArea").innerHTML = "<b>1 Poem Found</b><br><br>"; 
   } else {
-    document.getElementById("outputArea").innerHTML = "<b>" + data.length + " Poems Found</b>" + "<br>" + "<br>";
-
-    let answer = "";
-    for(let poem of data){
-      answer += "<hr>";
-      answer += " <b>Title:  </b>" + poem.title + "<br>";
-      answer += " <b>Author:  </b>" + poem.author + "<br>";
-      answer += "<b>Poem: </b><br><br>";
-
-      for(let line of poem.lines){
-        answer += "<i>" + line + "</i><br>";
-      }
-
-      answer += "<br>";
-    }
-    document.getElementById("outputArea").innerHTML += answer;
+    document.getElementById("outputArea").innerHTML = "<b>" + data.length + " Poems Found</b><br><br>";
   }
+
+  let answer = "";
+  for(let poem of data){
+    answer += "<hr>";
+    answer += " <b>Title:  </b>" + poem.title + "<br>";
+    answer += " <b>Author:  </b>" + poem.author + "<br>";
+    answer += "<b>Poem: </b><br><br>";
+
+    for(let line of poem.lines){
+      answer += "<i>" + line + "</i><br>";
+    }
+
+    answer += "<br>";
+  }
+  document.getElementById("outputArea").innerHTML += answer;
 }
